868d2180219b6e8be41db9f99554765808a62b9a
NeeleshBShetty <neeleshshetty@1236@gmail.com>
2023-09-26 14:15:05 +0530

    created MovieList file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import Movielist from "./MovieList";
 import Moviecard from "./Moviecard";
 
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <h1>Movie App</h1>
       <Moviecard />
+      <Movielist />
      
     </>
   );
